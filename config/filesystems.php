cb5c96834a0b839afcba6470c8f9e93b7d8df54d
bobih <bobby.khrisna@gmail.com>
2023-12-20 13:50:18 +0700

    pdate path

--- config/filesystems.php
+++ config/filesystems.php
@@ -34,13 +34,12 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('../../../public_html/images'),
+            'root' => storage_path('../../public_html/images'),
              'throw' => false,
         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('../../../public_html/images'),
             'url' => env('APP_URL').'/images',
             'visibility' => 'public',
             'throw' => false,
