2b0ab98e09c4bd4a447735722d9bb00fd3730565
bobih <bobby.khrisna@gmail.com>
2023-12-23 01:57:23 +0700

    add artisan optimize

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CronController;
+use App\Http\Controllers\ArtisanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::get("/crond",  [CronController::class, 'pushData']);
+Route::get("/artopt",  [ArtisanController::class, 'artisanOptimize']);
