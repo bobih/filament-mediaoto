f2520d9df11e5e5c2011d0224fd78c8a608467f0
bobih <bobby.khrisna@gmail.com>
2023-12-27 02:50:02 +0700

    update

--- app/Providers/Filament/DashPanelProvider.php
+++ app/Providers/Filament/DashPanelProvider.php
@@ -2,23 +2,24 @@
 
 namespace App\Providers\Filament;
 
-use App\Filament\AvatarProviders\CustomAvatarsProvider;
-use App\Filament\Resources\UserResource\Widgets\UserOverviewWidget;
-use Filament\Http\Middleware\Authenticate;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
 use Filament\Panel;
+use Filament\Widgets;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Filament\Http\Middleware\Authenticate;
+use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
-use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use App\Filament\AvatarProviders\CustomAvatarsProvider;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use App\Filament\Resources\UserResource\Widgets\UserOverviewWidget;
 
 class DashPanelProvider extends PanelProvider
 {
@@ -37,12 +38,9 @@ class DashPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
-
             ->plugins([
-                \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make()
+                FilamentShieldPlugin::make()
             ])
-
-
             ->defaultAvatarProvider(CustomAvatarsProvider::class)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
