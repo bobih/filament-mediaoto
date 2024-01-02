81db67a01c3d6408c232d8ddbada540a48a0f164
bobih <bobby.khrisna@gmail.com>
2024-01-02 20:28:35 +0700

    update Roles

--- app/Filament/Resources/UserResource/Pages/ViewUser.php
+++ app/Filament/Resources/UserResource/Pages/ViewUser.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
+use App\Filament\Resources\UserResource\RelationManagers;
 
 class ViewUser extends ViewRecord
 {
@@ -15,5 +16,22 @@ class ViewUser extends ViewRecord
         return [
            // UserResource\Widgets\ProspekInfoWidget::class,
         ];
+    }
+
+    public function getRelationManagers(): array
+    {
+        if(auth()->id() == $this->record->id &&  auth()->id() <> 36){
+            return [];
+        } else {
+            return [
+                RelationManagers\ProspekRelationManager::class,
+                RelationManagers\PushlistRelationManager::class,
+                RelationManagers\CalllistRelationManager::class,
+                RelationManagers\WhatsapplistRelationManager::class,
+                RelationManagers\LostRelationManager::class,
+
+            ];
+        }
+
     }
 }
