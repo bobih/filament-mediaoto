fc2d429a0f8226a26afac35dd2ca7ab7ead6842c
bobih <bobby.khrisna@gmail.com>
2024-01-07 21:59:00 +0700

    update

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use App\Models\Scopes\InvoiceScope;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -17,6 +20,15 @@ class Invoice extends Model implements HasMedia
     use InteractsWithMedia;
 
     protected $table = 'invoice';
+
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('preview')
+            ->fit(Manipulations::FIT_CROP, 300, 300)
+            ->nonQueued();
+    }
 
     protected function model(): Attribute
     {
