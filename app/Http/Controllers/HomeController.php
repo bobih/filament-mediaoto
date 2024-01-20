488b149645f7653f1d02abb8906f269d26af32c7
bobih <bobby.khrisna@gmail.com>
2024-01-20 15:33:08 +0700

    test loging

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\NewsPost;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
@@ -19,6 +20,9 @@ class HomeController extends Controller
         $response = NewsPost::wherein('id',[4,21,39,51,70,73])->orderBy('published_at','desc')->take(5)->get();
         //dd($response);
         //$response = [];
+
+        Log::info('Loading Home.');
+
         return view('home',[
             "posts" => $response
         ]);
