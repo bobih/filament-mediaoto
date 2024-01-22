84b75f647a522f5d319270f9fa2d33b0ca4151d0
bobih <bobby.khrisna@gmail.com>
2024-01-22 15:24:08 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\NewsPost;
 use Illuminate\Http\Request;
+use Spatie\Analytics\Period;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
+use Spatie\Analytics\Facades\Analytics;
 
 class HomeController extends Controller
 {
@@ -14,6 +16,9 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
+
+        $analyticsData = Analytics::fetchVisitorsAndPageViews(Period::days(7));
+        dd($analyticsData);
 
        // $response = Http::get('https://newsdata.io/api/1/news?country=id&category=technology,entertainment,education&size=5&apikey=pub_3644856b52db506d7f3ab3c51f9e2b20bde51')['results'];
         //dd($response);
