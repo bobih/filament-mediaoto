1625880f786cfe46354a55b7b59f55c30d836547
bobih <bobby.khrisna@gmail.com>
2024-01-25 15:52:53 +0700

    update banner

--- resources/views/layouts/widgets/banner.blade.php
+++ resources/views/layouts/widgets/banner.blade.php
@@ -4,11 +4,11 @@
         <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-5xl dark:text-white">Close more deals with single page contact management</h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Track stages and milestones of your deals to keep the sales process on track. Enjoy one click calling, appointment scheduling, and follow ups</p>
-            <button onclick="Livewire.dispatch('openModal', { component: 'contact-us',arguments: { menuevent: 'banner' }})"
+            <a href="javascript:void(0)" rel="noopener" target="_self" x-data={} x-on:click="$dispatch('open-modal')"
                 class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-400 rounded-lg hover:bg-white focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                 Get started
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </button>
+            </a>
         </div>
         <div class="mt-10 lg:mt-0 lg:col-span-5 lg:flex">
             <img src="https://www.mediaoto.id/images/phone-mockup2.png" alt="mediaoto-apps">
