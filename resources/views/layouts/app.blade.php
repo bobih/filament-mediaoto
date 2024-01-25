648074eaafcaf417df7b17ad53f877198918c2a7
bobih <bobby.khrisna@gmail.com>
2024-01-25 09:13:50 +0700

    update firebase

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,30 @@
         gtag('config', 'G-Q7LP278P3T');
     </script>
 
+<script type="module">
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+    import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-analytics.js";
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+      apiKey: "AIzaSyCjO4yYxDMZzKorD0dq4zZlNTmDMBzLgz8",
+      authDomain: "mediaoto-b3ac5.firebaseapp.com",
+      projectId: "mediaoto-b3ac5",
+      storageBucket: "mediaoto-b3ac5.appspot.com",
+      messagingSenderId: "676189219899",
+      appId: "1:676189219899:web:0deaa956dfaafb4eb0001e",
+      measurementId: "G-Q7LP278P3T"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  </script>
+
 <meta name="google-adsense-account" content="ca-pub-1433601050494794">
 
     <?php /* <script src="https://cdn.tailwindcss.com"></script> */ ?>
