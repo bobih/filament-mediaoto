5ab9906de6415ea11a5c6fd6a107b18a0fe9e2c5
bobih <bobby.khrisna@gmail.com>
2024-01-27 14:10:19 +0700

    Add Export

--- app/Filament/Resources/NewsCategoryResource.php
+++ app/Filament/Resources/NewsCategoryResource.php
@@ -84,6 +84,7 @@ class NewsCategoryResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
--- app/Filament/Resources/NewsPostResource/Pages/ListNewsPosts.php
+++ app/Filament/Resources/NewsPostResource/Pages/ListNewsPosts.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\NewsPostResource\Pages;
 use App\Filament\Resources\NewsPostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use pxlrbt\FilamentExcel\Columns\Column;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
 
 class ListNewsPosts extends ListRecords
 {
@@ -14,6 +17,34 @@ class ListNewsPosts extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+
+            ExportAction::make()->exports([
+                ExcelExport::make()->withColumns([
+                    Column::make('id')->heading('ID'),
+                    Column::make('created_at')->heading('Date'),
+                    Column::make('categories')->formatStateUsing(function ($record) {
+                        $list = '';
+                        foreach($record->categories as $category){
+                            $list .= $category->title . ", ";
+                        }
+                        return substr($list,0,-2);
+                     })->heading('Category'),
+                    Column::make('tags')->formatStateUsing(function ($record) {
+                        $list = '';
+                        foreach($record->tags as $tag){
+                            $list .= $tag->name . ", ";
+                        }
+                      return substr($list,0,-2);
+                     })->heading('Tags'),
+                    Column::make('author.nama')->heading('Author'),
+                    Column::make('title')->heading('Title'),
+                    Column::make('slug')->heading('URL')
+                    ->formatStateUsing(function ($state) {
+                       return env("APP_URL",'https://www.mediaoto.id').'/news/'. $state;
+                    }),
+                ])->withFilename('Site_News_' . date("Y-m-d")),
+            ])
+                ->color('info')
         ];
     }
 }
