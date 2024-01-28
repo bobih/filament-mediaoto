e08fd45319ba8d130d0ecf01ad8200055723f31e
bobih <bobby.khrisna@gmail.com>
2024-01-28 14:05:54 +0700

    custom Error 404

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     </script>
 
     <meta charset="utf-8">
+    <meta name="HandheldFriendly" content="true"/>
     <meta name="application-name" content="{{ config('app.name') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
