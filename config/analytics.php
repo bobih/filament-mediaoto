cfed0c956cdef3c689a3718b1246a9b69d0bfadd
bobih <bobby.khrisna@gmail.com>
2024-02-01 12:09:47 +0700

    update Analtics

--- config/analytics.php
+++ config/analytics.php
@@ -12,7 +12,7 @@ return [
      * to learn how to get this file. You can also pass the credentials as an array
      * instead of a file path.
      */
-    'service_account_credentials_json' => storage_path('app/analytics/mediaoto-b3ac5-f3104894220f.json'),
+    'service_account_credentials_json' => storage_path('app/analytics/mediaoto-b3ac5-d1fc34730712.json'),
 
     /*
      * The amount of minutes the Google API responses will be cached.
