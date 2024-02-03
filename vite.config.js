9edeea778f4c9c2ee3178ba469f6b4339faacb08
bobih <bobby.khrisna@gmail.com>
2024-02-03 15:15:10 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,6 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/css/filament/dash/theme.css',
-
             ],
             refresh: [
                 ...refreshPaths,
