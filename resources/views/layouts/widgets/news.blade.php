6d4f736d52a1378dc6f3873451c253cf1320ddf3
bobih <bobby.khrisna@gmail.com>
2024-02-05 19:17:46 +0700

    update wire:navigate

--- resources/views/layouts/widgets/news.blade.php
+++ resources/views/layouts/widgets/news.blade.php
@@ -22,7 +22,7 @@
                 <article
                     class="p-6 md:flex md:items-center bg-white rounded-lg border border-gray-300 shadow-md dark:bg-gray-800 dark:border-gray-700">
                     <div class="relative px-4 w-80 h-48 pb-1/4" style="min-width: 320px; ">
-                        <a title="{{ $post->title}}" href="{{ route('news.show', $post->slug) }}">
+                        <a wire:navigate title="{{ $post->title}}" href="{{ route('news.show', $post->slug) }}">
                             <img loading="lazy" class="absolute top-0 left-0 drop-shadow-lg right-0 bottom-0 h-full w-full  object-fit rounded-lg"
                                 src="{{ $post->getWebpthumb()}}" alt="{{ $post->title }}"
                                 title="{{ $post->title }}">
@@ -35,7 +35,7 @@
                         </div>
 
                         <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-300">
-                            <a title="{{$post->title}}" href="{{ route('news.show', $post->slug) }}">{{ $post->title }}</a>
+                            <a wire:navigate title="{{$post->title}}" href="{{ route('news.show', $post->slug) }}">{{ $post->title }}</a>
                         </h2>
                         <p class="mb-5 font-light text-gray-500 dark:text-gray-400">{{ $post->description }}</p>
            <div class="flex flex-wrap">
@@ -51,7 +51,7 @@
 
                     <!-- Arrow -->
                     <div class="px-5 text-center hidden lg:block">
-                        <a title="{{$post->title}}" href="{{ route('news.show', $post->slug) }}"
+                        <a wire:navigate title="{{$post->title}}" href="{{ route('news.show', $post->slug) }}"
                             class=" px-4 py-4 text-[#FF9119] border border-[#FF9119] hover:bg-[#FF9119] hover:text-white focus:ring-1 focus:outline-none focus:ring-[#FF9119] font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-[#FF9119] dark:text-[#FF9119] dark:hover:text-white  dark:hover:bg-[#FF9119]">
                             <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                                 viewBox="0 0 14 10">
