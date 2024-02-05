1d4556b94fc54d8bf4d006cb92a6cb4eaa535cd4
bobih <bobby.khrisna@gmail.com>
2024-02-05 18:39:17 +0700

    Update schema

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -255,7 +255,7 @@
 
 
 
-    <script src="https://cdn.tailwindcss.com"></script>
+    <?php /* <script src="https://cdn.tailwindcss.com"></script> */ ?>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 
