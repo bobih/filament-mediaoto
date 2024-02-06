aee18d703733acda2a30f82bedfbf48bbc9e4901
bobih <bobby.khrisna@gmail.com>
2024-02-07 00:59:57 +0700

    update theme

--- resources/views/components/news/news-detail.blade.php
+++ resources/views/components/news/news-detail.blade.php
@@ -13,7 +13,7 @@
                             <img class="mr-4 w-16 h-16 rounded-full" src="https://www.mediaoto.id/images/{{$post->author->image}}" alt="{{$post->author->nama}}">
                             <div>
                                 <a href="#" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">{{$post->author->nama}}</a>
-                                <p class="text-base text-gray-500 dark:text-gray-400">Content Creative Mediaoto</p>
+                                <p class="text-base text-gray-900 dark:text-gray-400">Content Creative Mediaoto</p>
                                 <?php /*
                                 <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate datetime="{{ $post->published_at }}" title="{{ $post->published_at }}">{{ $post->published_at }}</time></p>
                                 */ ?>
