aa4592fd2b524346e8ef5887df85fe276f52133c
bobih <bobby.khrisna@gmail.com>
2024-02-08 06:49:12 +0700

    update

--- app/Filament/Resources/CarBodyTypeResource.php
+++ app/Filament/Resources/CarBodyTypeResource.php
@@ -17,6 +17,7 @@ class CarBodyTypeResource extends Resource
 {
     protected static ?string $model = CarBodyType::class;
 
+
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
