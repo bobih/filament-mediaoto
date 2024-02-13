b3740062b10669a6c24b3c187c474e7bdb0fe6f0
bobih <bobby.khrisna@gmail.com>
2024-02-13 19:52:47 +0700

    update button type

--- resources/views/livewire/search-box.blade.php
+++ resources/views/livewire/search-box.blade.php
@@ -30,7 +30,7 @@
 
         */ ?>
 
-        <button id="btnsearch" title="search button" wire:loading.remove type="submit" onclick="handleSearch()"
+        <button id="btnsearch" title="search button" wire:loading.remove onclick="handleSearch()"
         class="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
 
         <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
