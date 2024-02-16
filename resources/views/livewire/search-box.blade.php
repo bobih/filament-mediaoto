c2423045164fde143235e855435a7e4bdba19923
bobih <bobby.khrisna@gmail.com>
2024-02-17 00:22:07 +0700

    update

--- resources/views/livewire/search-box.blade.php
+++ resources/views/livewire/search-box.blade.php
@@ -13,7 +13,7 @@
                 </svg>
             </div>
             */ ?>
-            <input type="search" name="search" id="search"
+            <input wire:model="search" type="search" name="search" id="search"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="{{__('news.place_search')}}" value='{{$this->search}}'>
         </div>
