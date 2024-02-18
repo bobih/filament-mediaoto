839051a799399cbe762cdc7052bfe214b68d6a71
bobih <bobby.khrisna@gmail.com>
2024-02-19 03:35:15 +0700

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -42,20 +42,6 @@ const firebaseConfig = {
     // ...
   });
 
-  onBackgroundMessage(messaging, (payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    // Customize notification here
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-      body: 'Background Message body.',
-      icon: '/firebase-logo.png'
-    };
-
-    self.registration.showNotification(notificationTitle,
-      notificationOptions);
-  });
-
-
 
 document.addEventListener('livewire:navigated', () => {
     console.log('navigated');
