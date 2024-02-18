51cb424e7c666a6097d768c352124a061371c51b
bobih <bobby.khrisna@gmail.com>
2024-02-18 08:52:28 +0700

    update

--- resources/views/layouts/widgets/products.blade.php
+++ resources/views/layouts/widgets/products.blade.php
@@ -1,6 +1,7 @@
 <div id="products" class="-mt-20 mb-20 "></div>
 
 @if (!$agent == 'mobile')
+<!-- Desktop -->
     <section class="bg-gray-100 dark:bg-gray-900 ">
         <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
             <div class="max-w-screen-md mb-8 lg:mb-16">
