0409d212728441ff82e4f86e0422cf4a57b86bbc
bobih <bobby.khrisna@gmail.com>
2024-02-18 22:48:31 +0700

    update

--- resources/views/components/news/news-item.blade.php
+++ resources/views/components/news/news-item.blade.php
@@ -4,7 +4,7 @@
     <div class="-mt-20 md:mt-0  md:px-4 pb-4 md:w-full" >
         <a wire:navigate title="{{ $post->title }}" alt="{{$post->title}}" href="{{ route('news.show', $post->slug) }}"
             class="md:hover:text-[#FF9119]">
-            <img  loading="lazy" class="md:hover:-translate-y-1 md:hover:scale-110 top-0 left-0 right-0 bottom-0 h-full w-full object-fit border dark:border-gray-700 shadow-xl rounded-lg duration-700"
+            <img  loading="lazy" class="md:hover:-translate-y-1 md:hover:scale-110 top-0 left-0 right-0 bottom-0 h-full w-full object-fit border dark:border-gray-700 shadow-xl rounded-lg duration-300"
                 src="{{ $post->getWebpthumb()}}" alt="{{ $post->slug }}">
         </a>
     </div>
