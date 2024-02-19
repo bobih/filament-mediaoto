1042213aba1a0704c80cb1130f4a1f3cc9d2cdf1
bobih <bobby.khrisna@gmail.com>
2024-02-20 04:49:29 +0700

    update

--- resources/views/layouts/widgets/banner.blade.php
+++ resources/views/layouts/widgets/banner.blade.php
@@ -36,7 +36,7 @@
             <div class="flex w-full justify-between ">
                 <div class="block md:hidden">
                     <button title="getstarted" x-data={} x-on:click="$dispatch('open-modal')"
-                        class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-400 rounded-lg hover:bg-white focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-400 dark:focus:ring-gray-800">
+                        class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-400 rounded-lg hover:bg-white focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                         {{__('home.banner.button1')}}
                         <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     </button>
