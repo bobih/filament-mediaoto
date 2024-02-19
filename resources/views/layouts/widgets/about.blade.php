a7fa94989a1710107430d310e2fd8d9e3265e1fb
bobih <bobby.khrisna@gmail.com>
2024-02-20 03:15:06 +0700

    update

--- resources/views/layouts/widgets/about.blade.php
+++ resources/views/layouts/widgets/about.blade.php
@@ -131,9 +131,9 @@
             </div>
         </div>
         <div class="mt-10 font-light text-gray-200 sm:text-lg dark:text-gray-400 lg:col-span-2 ">
-            <p class="mb-4 ">{{__('home.aboutus.content1')}}</p>
-            <p class="mb-4 ">{{__('home.aboutus.content2')}}</p>
-            <p class="mb-4 ">{{__('home.aboutus.content3')}}</p>
+            <p class="mb-4 text-justify">{{__('home.aboutus.content1')}}</p>
+            <p class="mb-4 text-justify">{{__('home.aboutus.content2')}}</p>
+            <p class="mb-4 text-justify">{{__('home.aboutus.content3')}}</p>
 
 
             <?php /*
