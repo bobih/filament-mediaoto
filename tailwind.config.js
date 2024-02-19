654cf2d528d48fadc0a5f032baec077ffc7dc062
bobih <bobby.khrisna@gmail.com>
2024-02-19 11:06:10 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,33 +30,9 @@ export default {
             'animate-[fade-in_0.5s_ease-in-out]'
                 ],
         extend: {
-            keyframes: {
-                lights: {
-                    '0%': {
-                      color: 'hsl(230, 40%, 80%)',
-                      dropShadow : '0 0 1em hsla(320, 100%, 50%, 0.2), 0 0 0.125em hsla(320, 100%, 60%, 0.3), -1em -0.125em 0.5em hsla(40, 100%, 60%, 0), 1em 0.125em 0.5em hsla(200, 100%, 60%, 0)',
-                    },
-                    '30%': {
-                      color: 'hsl(230, 80%, 90%)',
-                      dropShadow: '0 0 1em hsla(320, 100%, 50%, 0.5), 0 0 0.125em hsla(320, 100%, 60%, 0.5), -0.5em -0.125em 0.25em hsla(40, 100%, 60%, 0.2), 0.5em 0.125em 0.25em hsla(200, 100%, 60%, 0.4)',
-                    },
-                    '40%': {
-                      color: 'hsl(230, 100%, 95%)',
-                      dropShadow: '0 0 1em hsla(320, 100%, 50%, 0.5), 0 0 0.125em hsla(320, 100%, 90%, 0.5), -0.25em -0.125em 0.125em hsla(40, 100%, 60%, 0.2),0.25em 0.125em 0.125em hsla(200, 100%, 60%, 0.4)',
-                    },
-                    '70%': {
-                      color: 'hsl(230, 80%, 90%)',
-                      dropShadow: '0 0 1em hsla(320, 100%, 50%, 0.5), 0 0 0.125em hsla(320, 100%, 60%, 0.5), 0.5em -0.125em 0.25em hsla(40, 100%, 60%, 0.2),-0.5em 0.125em 0.25em hsla(200, 100%, 60%, 0.4)',
-                    },
-                    '100%': {
-                      color: 'hsl(230, 40%, 80%)',
-                      dropShadow: '0 0 1em hsla(320, 100%, 50%, 0.2),0 0 0.125em hsla(320, 100%, 60%, 0.3),1em -0.125em 0.5em hsla(40, 100%, 60%, 0),-1em 0.125em 0.5em hsla(200, 100%, 60%, 0)'
-                    },
-                    }
-                  },
             animation: {
-                lights: 'lights 5s 750ms linear infinite',
-                  },
+                'lights': 'lights 5s 750ms linear infinite',
+              },
             colors: {
                     danger: colors.rose,
                     primary: colors.blue,
