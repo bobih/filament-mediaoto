873cc62342ac5ca52e6d0f54e34996efb2d1bbab
bobih <bobby.khrisna@gmail.com>
2024-02-21 05:05:46 +0700

    update

--- resources/views/components/news/news-detail.blade.php
+++ resources/views/components/news/news-detail.blade.php
@@ -29,7 +29,7 @@
                     @endif
 
                     <header class="mb-4 lg:mb-6 not-format">
-                        <div class="w-full h-auto pb-1/4">
+                        <div x-on:click="isImageModalOpen = true" class="w-full h-auto pb-1/4">
                             <img class="h-auto w-full object-fit drop-shadow-xl rounded-lg "
                                 src="{{ $post->getWebp()}}" alt="{{ $post->slug }}">
                         </div>
@@ -110,4 +110,26 @@
             </div>
         </main>
     </div>
+    <div
+  class="modal"
+  role="dialog"
+  tabindex="-1"
+  x-show="isImageModalOpen"
+  x-on:click.away="isImageModalOpen = false"
+  x-cloak
+  x-transition
+>
+    <div class="model-inner">
+      <div class="modal-header">
+        <h3>Hello World</h3>
+        <button aria-label="Close" x-on:click="isImageModalOpen=false">✖</button>
+      </div>
+      <p>
+        Natus earum velit ab nobis eos. Sed et exercitationem voluptatum omnis
+        dolor voluptates. Velit ut ipsam sunt ipsam nostrum. Maiores officia
+        accusamus qui sapiente. Dolor qui vel placeat dolor nesciunt quo dolor
+        dolores. Quo accusamus hic atque nisi minima.
+      </p>
+    </div>
+</div>
 </div>
