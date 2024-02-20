b2bfc7329fdf3b48c7b5188697cb5df85abafc31
bobih <bobby.khrisna@gmail.com>
2024-02-21 04:23:03 +0700

    update

--- app/Livewire/NewsList.php
+++ app/Livewire/NewsList.php
@@ -36,17 +36,13 @@ class NewsList extends Component
         $agent = new Agent();
         // Implement cache
         if($this->search == '' && $this->category == '' && $this->tag == ''){
-            /*
+
              $response = Cache::remember('newsSearchResponse', Carbon::now()->addDay(), function () {
                 return NewsPost::with('categories','media','tags','author')
                 ->published()
-                ->orderBy('published_at','desc')->get();
+                ->orderBy('published_at','desc');
             });
-            */
 
-            $response = NewsPost::with('categories','media','tags','author')
-            ->published()
-            ->orderBy('published_at','desc');
 
         } else {
             $response = NewsPost::where('title', 'LIKE', "%".$this->search."%")
