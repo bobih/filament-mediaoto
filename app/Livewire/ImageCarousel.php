f80f303ddc0b2aa1817aab600ba78a74fe4bb619
bobih <bobby.khrisna@gmail.com>
2024-02-21 06:17:10 +0700

    update

--- app/Livewire/ImageCarousel.php
+++ app/Livewire/ImageCarousel.php
@@ -3,10 +3,11 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\On;
 
 class ImageCarousel extends Component
 {
-
+    #[On('showImageModal')]
     public function showImageModal(){
         $this->dispatch('open-image-modal');
     }
