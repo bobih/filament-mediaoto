d9e6972196a7b70d258f7a8736def5e0fa9bbcc9
bobih <bobby.khrisna@gmail.com>
2024-02-21 05:07:42 +0700

    update

--- resources/views/components/news/news-detail.blade.php
+++ resources/views/components/news/news-detail.blade.php
@@ -110,26 +110,5 @@
             </div>
         </main>
     </div>
-    <div
-  class="modal"
-  role="dialog"
-  tabindex="-1"
-  x-show="isImageModalOpen"
-  x-on:click.away="isImageModalOpen = false"
-  x-cloak
-  x-transition
->
-    <div class="model-inner">
-      <div class="modal-header">
-        <h3>Hello World</h3>
-        <button aria-label="Close" x-on:click="isImageModalOpen=false">✖</button>
-      </div>
-      <p>
-        Natus earum velit ab nobis eos. Sed et exercitationem voluptatum omnis
-        dolor voluptates. Velit ut ipsam sunt ipsam nostrum. Maiores officia
-        accusamus qui sapiente. Dolor qui vel placeat dolor nesciunt quo dolor
-        dolores. Quo accusamus hic atque nisi minima.
-      </p>
-    </div>
-</div>
+
 </div>
