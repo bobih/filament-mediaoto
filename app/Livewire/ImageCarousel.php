472db4674739b1a44fcb33e61c74cb342248b901
bobih <bobby.khrisna@gmail.com>
2024-02-21 06:23:45 +0700

    update

--- app/Livewire/ImageCarousel.php
+++ app/Livewire/ImageCarousel.php
@@ -2,19 +2,29 @@
 
 namespace App\Livewire;
 
+use App\Models\NewsPost;
 use Livewire\Component;
 use Livewire\Attributes\On;
 
 class ImageCarousel extends Component
 {
+    public $images = '';
+
     #[On('showImageModal')]
     public function showImageModal($postid){
-        dd($postid);
+
+        $newsdata = NewsPost::where('id',$postid);
+        dd($newsdata);
+
+
+
         $this->dispatch('open-image-modal');
     }
 
     public function render()
     {
-        return view('livewire.image-carousel');
+        return view('livewire.image-carousel',[
+            'images' => $this->images
+        ]);
     }
 }
