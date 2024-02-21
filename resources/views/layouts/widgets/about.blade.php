a5d7a103b3035a658b0fbd6458fee8c11e036f2c
bobih <bobby.khrisna@gmail.com>
2024-02-22 05:47:52 +0700

    q

--- resources/views/layouts/widgets/about.blade.php
+++ resources/views/layouts/widgets/about.blade.php
@@ -87,7 +87,7 @@
                         </g>
                     </svg>
                 </a>
-
+                <?php /*
                 <a target="_blank" title="tiktok" rel="noopener noreferrer" href="https://www.tiktok.com/@mediaoto.id" class="hover:text-gray-300">
                     <svg class="w-12 h-12 fill-current" width="24px" height="24px" viewBox="0 0 24 24"
                         xmlns="http://www.w3.org/2000/svg">
@@ -95,9 +95,13 @@
                             d="M21,2H3A1,1,0,0,0,2,3V21a1,1,0,0,0,1,1H21a1,1,0,0,0,1-1V3A1,1,0,0,0,21,2Zm-3.281,8.725h0c-.109.011-.219.016-.328.017A3.571,3.571,0,0,1,14.4,9.129v5.493a4.061,4.061,0,1,1-4.06-4.06c.085,0,.167.008.251.013v2a2.067,2.067,0,1,0-.251,4.119A2.123,2.123,0,0,0,12.5,14.649l.02-9.331h1.914A3.564,3.564,0,0,0,17.719,8.5Z" />
                     </svg>
                 </a>
+                */ ?>
 
-                <a class="hover:text-gray-300">
-                    <svg class="w-12 h-12" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M17 1H3c-1.1 0-2 .9-2 2v14c0 1.101.9 2 2 2h7v-7H8V9.525h2v-2.05c0-2.164 1.212-3.684 3.766-3.684l1.803.002v2.605h-1.197c-.994 0-1.372.746-1.372 1.438v1.69h2.568L15 12h-2v7h4c1.1 0 2-.899 2-2V3c0-1.1-.9-2-2-2z"></path></svg>
+                <a target="_blank" title="tiktok" rel="noopener noreferrer" href="https://www.facebook.com/profile.php?id=61556552852537" class="hover:text-gray-300">
+
+                    <svg class="w-12 h-12" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M17 1H3c-1.1 0-2 .9-2 2v14c0 1.101.9 2 2 2h7v-7H8V9.525h2v-2.05c0-2.164 1.212-3.684 3.766-3.684l1.803.002v2.605h-1.197c-.994 0-1.372.746-1.372 1.438v1.69h2.568L15 12h-2v7h4c1.1 0 2-.899 2-2V3c0-1.1-.9-2-2-2z"></path>
+                    </svg>
                 </a>
 
 
