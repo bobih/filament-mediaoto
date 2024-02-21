264754e409bd3fb1fd1e8ae90adbd7f3916aecb9
bobih <bobby.khrisna@gmail.com>
2024-02-22 02:11:28 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->app->setLocale('id');
+
         /*
         Filament::serving(function () {
             Filament::registerNavigationGroups([
