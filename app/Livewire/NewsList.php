5967ee301baedec1e9041047ee0a6ab7b86b8846
bobih <bobby.khrisna@gmail.com>
2024-02-22 04:26:21 +0700

    q

--- app/Livewire/NewsList.php
+++ app/Livewire/NewsList.php
@@ -57,12 +57,7 @@ class NewsList extends Component
                 $response = NewsPost::with('categories','media','tags','author')
                 ->published()
                 ->orderBy('published_at','desc')
-                ->when(NewsCategory::where('slug',$this->category)->first(), function($query){
-                    $query->withCategory($this->category);
-                })
-                ->when(NewsPost::withAllTags([$this->tag])->first(), function($query){
-                    $query->withAllTags([$this->search]);
-                });
+                ->when(NewsPost::withAllTags([$this->search]));
             }
             $response = $response->paginate($this->perPage);
        return $response;
