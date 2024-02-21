e8a4c19909b5df8c9d6154cf3edb73635cccb54b
bobih <bobby.khrisna@gmail.com>
2024-02-21 23:07:40 +0700

    update

--- resources/views/components/custom-carousel.blade.php
+++ resources/views/components/custom-carousel.blade.php
@@ -60,18 +60,14 @@
 
                @foreach ($slider as $slide)
                 <div class="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-                    data-te-carousel-item data-te-carousel-active>
+                    data-te-carousel-item>
                     <img src="{{$slide->image}}" class="block w-full"
                         alt="Wild Landscape" />
                 </div>
                @endforeach
                 <!--First item-->
 
-                <!--Second item-->
-                <div class="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-                    data-te-carousel-item>
-                    <img src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp" class="block w-full" alt="Camera" />
-                </div>
+
 
             </div>
 
