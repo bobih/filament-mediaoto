85dad85acd62b38dda9f262f187e65a0b01bedcd
bobih <bobby.khrisna@gmail.com>
2024-02-22 03:47:45 +0700

    update

--- app/Http/Controllers/NewsPostController.php
+++ app/Http/Controllers/NewsPostController.php
@@ -257,7 +257,7 @@ class NewsPostController extends Controller
                 dd($arrSearch);
                 $newsResponse =  NewsPost::where(function($query) use ($arrSearch) {
                     foreach ($arrSearch as $value) {
-                        $query->orWhere('title', 'like', "%{$value}%");
+                        $query->orWhere('title', 'like', "%".$value."%");
                     }
                 })
                 ->with('categories', 'media', 'tags', 'author')
