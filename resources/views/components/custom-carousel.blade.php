399201b93a6d105565a1687e5d40bfbe34baad6e
bobih <bobby.khrisna@gmail.com>
2024-02-21 20:03:58 +0700

    update

--- resources/views/components/custom-carousel.blade.php
+++ resources/views/components/custom-carousel.blade.php
@@ -54,7 +54,7 @@
                 */ ?>
                 <div id="custom-controls-gallery" class="relative w-full" data-carousel="static">
                     <!-- Carousel wrapper -->
-                    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                    <div class="relative h-[560px] overflow-hidden rounded-lg md:h-96">
                          <!-- Item 1 -->
                         <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
                             <img src="{{$slider}}" class="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="">
