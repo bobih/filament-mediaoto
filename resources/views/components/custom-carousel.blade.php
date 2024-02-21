fa0b9a3dca1f42a0231b799b34e64b86d0ac5ffe
bobih <bobby.khrisna@gmail.com>
2024-02-21 20:21:40 +0700

    update

--- resources/views/components/custom-carousel.blade.php
+++ resources/views/components/custom-carousel.blade.php
@@ -54,12 +54,12 @@
                     </button>
                 </div>
                 */ ?>
-                <div id="indicators-carousel" class="relative w-full" data-carousel="static">
+                <div id="indicators-carousel" class="relative w-full min-h-full" data-carousel="static">
                     <!-- Carousel wrapper -->
                     <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
                          <!-- Item 1 -->
                         <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
-                            <img src="{{$slider}}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                            <img src="{{$slider}}" class="absolute block w-full " alt="...">
                         </div>
                         <!-- Item 2 -->
                         <div class="hidden duration-700 ease-in-out" data-carousel-item>
