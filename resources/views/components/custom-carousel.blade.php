c854fd10a2479e764e3ba824d29434fa710187d8
bobih <bobby.khrisna@gmail.com>
2024-02-21 22:28:11 +0700

    update

--- resources/views/components/custom-carousel.blade.php
+++ resources/views/components/custom-carousel.blade.php
@@ -68,12 +68,7 @@
                     data-te-carousel-item>
                     <img src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp" class="block w-full" alt="Camera" />
                 </div>
-                <!--Third item-->
-                <div class="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-                    data-te-carousel-item>
-                    <img src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp" class="block w-full"
-                        alt="Exotic Fruits" />
-                </div>
+
             </div>
 
             <!--Carousel controls - prev item-->
