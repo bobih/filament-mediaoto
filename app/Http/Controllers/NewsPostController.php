b3a2679461badce439fbb1f87061da5b1a0a6a34
bobih <bobby.khrisna@gmail.com>
2024-02-21 23:00:46 +0700

    update

--- app/Http/Controllers/NewsPostController.php
+++ app/Http/Controllers/NewsPostController.php
@@ -124,7 +124,8 @@ class NewsPostController extends Controller
                 $metaProduct = $controller->getMetaProduct($news->car_model, $news);
             }
 
-            $slider = $news->media[0]->getUrl();;
+            $slider = [];
+            $slider[0] = $news->media[0]->getUrl();;
 
 
             $newsid =  $news;
