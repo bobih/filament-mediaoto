1915646afcb52c1056dd8ad8ade6aa03773bc459
bobih <bobby.khrisna@gmail.com>
2024-02-22 04:37:10 +0700

    update search

--- lang/en/news.php
+++ lang/en/news.php
@@ -4,7 +4,7 @@ return [
     "title" => "Your Source for Cutting-Edge News in Cars and Real Estate - Mediaoto",
     "description" => "Discover the pulse of automotive and property realms on Mediaoto.id. Uncover listings, updates, and trends in automotive and property, always ahead in news and prices!",
     "recommended" => "Recommended Topics",
-    "place_search" => "Search tips, trick, etc...",
+    "place_search" => "Search toyota, honda, etc...",
     "related_news" => "Related News",
 ];
 
--- lang/id/news.php
+++ lang/id/news.php
@@ -4,6 +4,6 @@ return [
     "title" => "Berita Otomotif dan berita rumah Terbaru  | mediaoto.id",
     "description" => "Mediaoto.id menampilkan semua berita terbaru dari otomotif, properti. mobil dan rumah di jual,update dan harga terbaru yang akan datang",
     "recommended" => "Topik Pilihan",
-    "place_search" => "Cari tips, trick, dll...",
+    "place_search" => "Cari toyota, honda, dll...",
     "related_news" => "Berita Terkait",
 ];
