b57de9cc24b6571a2efc353d92b553b7c690be9e
bobih <bobby.khrisna@gmail.com>
2024-02-21 20:19:24 +0700

    update

--- resources/views/components/custom-carousel.blade.php
+++ resources/views/components/custom-carousel.blade.php
@@ -59,7 +59,7 @@
                     <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
                          <!-- Item 1 -->
                         <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
-                            <img src="{{$images}}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                            <img src="{{$slider}}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                         </div>
                         <!-- Item 2 -->
                         <div class="hidden duration-700 ease-in-out" data-carousel-item>
