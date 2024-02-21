d9bd7b1591c5026dd7a9c824b182fc121b0c4241
bobih <bobby.khrisna@gmail.com>
2024-02-21 20:49:53 +0700

    up

--- resources/views/livewire/image-carousel.blade.php
+++ resources/views/livewire/image-carousel.blade.php
@@ -8,7 +8,7 @@
                 <img src="{{$this->images}}" class="absolute mx-auto block h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
             </div>
             <!-- Item 5 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="{{$this->images}}" class="absolute mx-auto block h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
             </div>                    </div>
         <!-- Slider controls -->
