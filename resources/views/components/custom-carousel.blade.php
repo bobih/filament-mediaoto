c5fc7aa9d182cbade821fe4e70257bcd711aca87
bobih <bobby.khrisna@gmail.com>
2024-02-23 02:07:48 +0700

    q

--- resources/views/components/custom-carousel.blade.php
+++ resources/views/components/custom-carousel.blade.php
@@ -24,12 +24,12 @@
         x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0"
         x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200"
         x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
-        <div class="absolute inset-0 bg-gray-500 dark:bg-gray-900 opacity-75"></div>
+        <div class="absolute inset-0 bg-gray-500 dark:bg-gray-900 opacity-85"></div>
     </div>
 
     <div x-show="show"
         style="max-height: 500px"
-        class="mb-6 bg-gray-100 dark:bg-gray-800/50 rounded-lg  shadow-xl transform transition-all h-full  w-full sm:mx-auto"
+        class="mb-6  transform transition-all h-full  w-full sm:mx-auto"
         x-trap.inert.noscroll="show" x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200"
