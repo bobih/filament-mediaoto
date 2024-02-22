b4f412f26011302e6dd7666e2a33c28e1cc78575
bobih <bobby.khrisna@gmail.com>
2024-02-23 04:21:27 +0700

    q

--- resources/views/components/news/news-item.blade.php
+++ resources/views/components/news/news-item.blade.php
@@ -1,5 +1,5 @@
 @props(['post', 'categories'])
-<article
+<article wire:key="{{$post->id}}"
     class="mt-20 md:mt-0 p-6 md:flex md:items-center bg-white rounded-lg border border-gray-300 shadow-md dark:bg-gray-800 dark:border-gray-700 ">
     <div class="-mt-20 md:mt-0  md:px-4 pb-4 md:w-full" >
         <a wire:navigate title="{{ $post->title }}" alt="{{$post->title}}" href="{{ route('news.show', $post->slug) }}"
