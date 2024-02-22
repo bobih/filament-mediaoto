16a629f6ebf9e24bd6bccb14cf1b4431b22d8ae7
bobih <bobby.khrisna@gmail.com>
2024-02-23 04:36:01 +0700

    q

--- app/Http/Controllers/NewsPostController.php
+++ app/Http/Controllers/NewsPostController.php
@@ -59,6 +59,7 @@ class NewsPostController extends Controller
                 $query->published();
             })->take(10)->get();
         }
+        $newsResponse = [];
 
 
         return view('news.index', [
