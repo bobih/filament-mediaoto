b8b7b177285f9dc78a0c00b45916f434ec1d35c4
bobih <bobby.khrisna@gmail.com>
2024-02-23 05:07:21 +0700

    up

--- resources/views/layouts/widgets/banner.blade.php
+++ resources/views/layouts/widgets/banner.blade.php
@@ -87,7 +87,7 @@
         data-te-animation="[fly-in-left_0.5s]"
         data-te-animation-show-on-load="false"
          class="z-10 md:hidden mt-10 lg:mt-0 lg:col-span-5 lg:flex">
-            <img class="" src="https://www.mediaoto.id/images/phone-mockup-mobile.webp" alt="mediaoto-apps" title="mediaoto-apps">
+            <img class="" src="{{env('IMAGE_URL','https://www.mediaoto.id')}}/images/phone-mockup-mobile.webp" alt="mediaoto-apps" title="mediaoto-apps">
         </div>
 
 
@@ -112,7 +112,7 @@
         data-te-animation="[fly-in-left_0.5s]"
         data-te-animation-show-on-load="false"
          class="z-10 hidden md:block mt-10 lg:mt-0 lg:col-span-5 lg:flex">
-            <img class="" src="https://www.mediaoto.id/images/phone-mockup-desktop.webp" alt="mediaoto-apps" title="mediaoto-apps">
+            <img class="" src="{{env('IMAGE_URL','https://www.mediaoto.id')}}/images/phone-mockup-desktop.webp" alt="mediaoto-apps" title="mediaoto-apps">
         </div>
         @endif
     </div>
