674f627396127788a82d1ce1dc60206813048ccf
bobih <bobby.khrisna@gmail.com>
2024-02-22 14:41:11 +0700

    q

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,8 +29,9 @@ class HomeController extends Controller
     {
         $lang = $request->session()->get('applocale');
 
-        dd($lang);
-
+        if($lang == null){
+            $lang = 'id';
+        }
         if (array_key_exists($lang, Config::get('languages'))) {
             Session::put('applocale', 'id');
             app()->setLocale('id');
