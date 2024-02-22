2f0a28ff0c92d2ee14e980950147df43501fe606
bobih <bobby.khrisna@gmail.com>
2024-02-23 01:53:59 +0700

    q

--- resources/views/components/custom-carousel.blade.php
+++ resources/views/components/custom-carousel.blade.php
@@ -66,11 +66,10 @@
                 @if ($first)
                 <div  class="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
                     data-te-carousel-item data-te-carousel-active>
-                    <div class="bg-contain bg-center block mx-auto" style="background-image: url({{$slide->image}})"></div>
-                    <?php /*
-                    <img src="{{$slide->image}}" class="block mx-auto"
+
+                    <img src="{{$slide->image}}" class="block object-fill mx-auto"
                         alt="Wild Landscape" />
-                        */ ?>
+
                 </div>
                 @php
                     $first = false;
