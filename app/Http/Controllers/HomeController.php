30d713ba0e4737859153ac8b9190135eec70761c
bobih <bobby.khrisna@gmail.com>
2024-02-22 14:53:12 +0700

    update lang

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,16 +27,6 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-        $lang = Session::get('applocale');
-
-       // dd($lang);
-        if($lang == null){
-            $lang = 'id';
-
-            Session::put('applocale', 'id');
-            app()->setLocale('id');
-
-        }
 
        // $response = Http::get('https://newsdata.io/api/1/news?country=id&category=technology,entertainment,education&size=5&apikey=pub_3644856b52db506d7f3ab3c51f9e2b20bde51')['results'];
         //dd($response);
