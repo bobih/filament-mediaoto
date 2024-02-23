4eaca8c91eefed16aa5295ff26dc3d1e723a34c8
bobih <bobby.khrisna@gmail.com>
2024-02-24 05:59:11 +0700

    update

--- app/Http/Controllers/FcmController.php
+++ app/Http/Controllers/FcmController.php
@@ -399,7 +399,6 @@ class FcmController extends Controller
 
     public function sentContactUs($request){
 
-        dd($request);
 
         $userToken = "e28hxY1XmkwUMUU2ITOdHj:APA91bH0FWU1iKNXdjn3fZK76odmaQCITZp6inlFyVf_HgCFtOHYTzBQzfDPsnMIe-mXBeat4byEX7y0Nz1x94EJSmB9u3yQrkuaiFZxEMHeiDyfeFrT-AP4SqHhxVminrHoaP9fBYf8";
         $body = " Nama: " .   $request['name'];
