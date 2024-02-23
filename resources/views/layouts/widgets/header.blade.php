a2c87bafc14ad1a196299c37f6b31db0a1679228
bobih <bobby.khrisna@gmail.com>
2024-02-23 12:43:44 +0700

    up

--- resources/views/layouts/widgets/header.blade.php
+++ resources/views/layouts/widgets/header.blade.php
@@ -187,7 +187,7 @@
                     <a title="headerlogo" href="/#home" rel="noopener" target="_self"
                         class="flex items-center gap-3 text-gray-500 dark:text-gray-400  hover:text-gray-900 dark:hover:text-white">
                         <!-- <img src="/images/white_logo.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
-                        <x-application-logo :size="7" />
+                        <x-application-logo :size="6" />
                         <span class="self-center text-lg font-semibold whitespace-nowrap">Mediaoto</span>
                     </a>
                 @else
