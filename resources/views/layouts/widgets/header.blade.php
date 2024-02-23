ddd618a9395115414fa4310180d5e539f68aa7f5
bobih <bobby.khrisna@gmail.com>
2024-02-24 01:23:17 +0700

    update

--- resources/views/layouts/widgets/header.blade.php
+++ resources/views/layouts/widgets/header.blade.php
@@ -102,23 +102,33 @@
                               $arrMenu = array(
                                 'home' => array(
                                     "pagename" => "home",
+                                    "class" => '',
                                     "url"   => "/",
                                     "href"  => "/#home",
                                     "lang"  => "home.nav.home"
                                 )
                               );
+                                $x = 0;
+                              foreach ($arrMenu as $menu) {
+                                if($this->nav == $menu['pagename']){
+                                    $arrMenu[$x]['class'] = 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 dark:border-indigo-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out';
+                                } else {
+                                    $arrMenu[$x]['class'] = 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out';
+                                }
+                                $x++;
+                              }
                               //dd($arrMenu);
                             @endphp
                         @foreach ($arrMenu as $menu )
                         <li>
                             @if (Request::segment(1) == '')
                             <a title="home" href="{{$menu['href']}}" wire:click.prevent="pageName({{$menu['pagename']}})" rel="noopener" target="_self"
-                                class="navlink block py-2 px-3 text-gray-900 rounded hover:bg-white md:hover:bg-transparent md:hover:text-[#FF9119] md:p-0 dark:text-white md:dark:hover:text-[#FF9119] dark:hover:bg-gray-700 dark:focus:text-[#FF9119] dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                class="{{$menu['class']}}">
                                 {{ __($menu['lang']) }}
                             </a>
                         @else
                             <button wire:click.prevent="getPage('/','home')"
-                                class="navlink block py-2 px-3 text-gray-900 rounded hover:bg-white md:hover:bg-transparent md:hover:text-[#FF9119] md:p-0 dark:text-white md:dark:hover:text-[#FF9119] dark:hover:bg-gray-700 dark:focus:text-[#FF9119] dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                class="{{$menu['class']}}">
                                 {{ __('home.nav.home') }}
                             </button>
                         @endif
