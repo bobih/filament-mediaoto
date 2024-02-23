184030cd4de5975915ec1dd56478f40a7ad2c530
bobih <bobby.khrisna@gmail.com>
2024-02-24 02:43:03 +0700

    update

--- resources/views/components/custom-carousel.blade.php
+++ resources/views/components/custom-carousel.blade.php
@@ -91,7 +91,7 @@
             <button
                 class="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
                 type="button" data-te-target="#carouselExampleControls" data-te-slide="prev">
-                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-600 group-hover:bg-white/50 dark:group-hover:bg-gray-600 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-600 group-hover:bg-white/50 dark:group-hover:bg-gray-200 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                     <svg class="w-4 h-4 text-gray-600 dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
                     </svg>
@@ -105,7 +105,7 @@
             <button
                 class="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
                 type="button" data-te-target="#carouselExampleControls" data-te-slide="next">
-                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full  bg-white/30 dark:bg-gray-600 group-hover:bg-white/50 dark:group-hover:bg-gray-600 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full  bg-white/30 dark:bg-gray-600 group-hover:bg-white/50 dark:group-hover:bg-gray-200 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                     <svg class="w-4 h-4 text-gray-600 dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
                     </svg>
