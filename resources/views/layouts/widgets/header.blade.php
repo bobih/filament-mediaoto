c5a6407f8930604a29e0e7af1105df600d421ebc
bobih <bobby.khrisna@gmail.com>
2024-02-24 01:29:10 +0700

    update

--- resources/views/layouts/widgets/header.blade.php
+++ resources/views/layouts/widgets/header.blade.php
@@ -99,7 +99,8 @@
                     <ul
                         class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-400 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:bg-gray-800">
                         @php
-                              $arrMenu =  array(
+
+                              $arrMenu[] =  array(
                                     "pagename" => "home",
                                     "class" => '',
                                     "url"   => "/",
