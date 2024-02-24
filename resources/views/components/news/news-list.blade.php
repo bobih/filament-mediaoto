d93d340280c4c88c976a1adc28f2594346eb7416
bobih <bobby.khrisna@gmail.com>
2024-02-25 06:26:57 +0700

    update

--- resources/views/components/news/news-list.blade.php
+++ resources/views/components/news/news-list.blade.php
@@ -71,7 +71,7 @@
             </div>
         </div>
     </div>
-    @script
+
 <script wire:ignore>
     document.addEventListener("DOMContentLoaded", () => {
 
@@ -80,6 +80,6 @@
         });
     });
   </script>
-@endscript
+
 </section>
 
