17fab158c836a9d0877d861d0261b947b27eb3d4
bobih <bobby.khrisna@gmail.com>
2024-02-26 01:28:06 +0700

    update

--- resources/views/components/news/news-item.blade.php
+++ resources/views/components/news/news-item.blade.php
@@ -4,6 +4,9 @@
         observe () {
             let observer = new IntersectionObserver((entries) => {
                 entries.forEach(entry => {
+                    const manually = new Animate(manuallyEl, {
+
+                    });
                     if (entry.isIntersecting) {
                         console.log('intersect..')
                     }
@@ -19,10 +22,6 @@
 
     }"
     x-init="observe"
-        data-te-animation-init
-        data-te-animation-start="onScroll"
-        data-te-animation="[fly-in-left_0.5s]"
-        data-te-animation-show-on-load="true"
         class="mt-20 md:mt-0 p-6 md:flex md:items-center bg-white rounded-lg border border-gray-300 shadow-md dark:bg-gray-800 dark:border-gray-700">
     <div class="-mt-20 md:mt-0  md:px-4 pb-4 md:w-full" >
         <a wire:navigate title="{{ $post->title }}" alt="{{$post->title}}" href="{{ route('news.show', $post->slug) }}"
