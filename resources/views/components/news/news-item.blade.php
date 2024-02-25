3a9d13b57590b91f640efdacf51a4b206fe4c1de
bobih <bobby.khrisna@gmail.com>
2024-02-25 23:07:04 +0700

    add animation

--- resources/views/components/news/news-item.blade.php
+++ resources/views/components/news/news-item.blade.php
@@ -1,5 +1,13 @@
 @props(['post', 'categories'])
 <article wire:key="{{$post->id}}"
+
+    id="animate"
+        data-te-animation-init
+        data-te-animation-reset="true"
+        data-te-animation-start="manually"
+        data-te-animation="[fly-in-left_0.5s]"
+        data-te-animation-show-on-load="false"
+
     class="mt-20 md:mt-0 p-6 md:flex md:items-center bg-white rounded-lg border border-gray-300 shadow-md dark:bg-gray-800 dark:border-gray-700">
     <div class="-mt-20 md:mt-0  md:px-4 pb-4 md:w-full" >
         <a wire:navigate title="{{ $post->title }}" alt="{{$post->title}}" href="{{ route('news.show', $post->slug) }}"
