2c268830cefa44247fd51a0f4a8a26d41a3dfcac
bobih <bobby.khrisna@gmail.com>
2024-02-26 00:26:53 +0700

    up

--- resources/views/components/news/news-item.blade.php
+++ resources/views/components/news/news-item.blade.php
@@ -32,10 +32,10 @@
     x-init="observe"
     id="animate"
         data-te-animation-init
-        data-te-animation-reset="true"
         data-te-animation-start="manually"
+        data-te-animation-reset="true"
         data-te-animation="[fly-in-left_0.5s]"
-        data-te-animation-show-on-load="true"
+
     class="mt-20 md:mt-0 p-6 md:flex md:items-center bg-white rounded-lg border border-gray-300 shadow-md dark:bg-gray-800 dark:border-gray-700">
     <div class="-mt-20 md:mt-0  md:px-4 pb-4 md:w-full" >
         <a wire:navigate title="{{ $post->title }}" alt="{{$post->title}}" href="{{ route('news.show', $post->slug) }}"
