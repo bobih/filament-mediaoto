929af3294a28211d6a7d371d36d638d6bf3bd2c2
bobih <bobby.khrisna@gmail.com>
2024-02-26 00:38:38 +0700

    up

--- resources/views/components/news/news-item.blade.php
+++ resources/views/components/news/news-item.blade.php
@@ -30,13 +30,13 @@
         }
     }"
     x-init="observe"
-    id="animate"
+        id="animate"
         data-te-animation-init
-        data-te-animation-start="manually"
         data-te-animation-reset="true"
-        data-te-animation="[browse-in_0.5s_1s_ease-in-out]"
-
-    class="mt-20 md:mt-0 p-6 md:flex md:items-center bg-white rounded-lg border border-gray-300 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        data-te-animation-start="manually"
+        data-te-animation="[browse-in_1s_ease-in-out]"
+        data-te-animation-show-on-load="false"
+        class="mt-20 md:mt-0 p-6 md:flex md:items-center bg-white rounded-lg border border-gray-300 shadow-md dark:bg-gray-800 dark:border-gray-700">
     <div class="-mt-20 md:mt-0  md:px-4 pb-4 md:w-full" >
         <a wire:navigate title="{{ $post->title }}" alt="{{$post->title}}" href="{{ route('news.show', $post->slug) }}"
             class="md:hover:text-[#FF9119]">
