9d2488f206d1aa2bf89f57bca5c5224a59053991
bobih <bobby.khrisna@gmail.com>
2024-02-25 08:09:16 +0700

    update

--- app/Livewire/NewsList.php
+++ app/Livewire/NewsList.php
@@ -36,7 +36,7 @@ class NewsList extends Component
         $agent = new Agent();
 
 
-        $response = NewsPost::where('title', 'LIKE', "%".$this->search."%")
+        $response = NewsPost::where('content', 'LIKE', "%".$this->search."%")
                     ->with('categories','media','tags','author')
                     ->published()
                     ->orderBy('published_at','desc')
