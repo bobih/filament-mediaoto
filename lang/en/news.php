a71b972335f2f7760700d7ddf2ffe1419a3216ca
bobih <bobby.khrisna@gmail.com>
2024-02-26 11:55:53 +0700

    update

--- lang/en/news.php
+++ lang/en/news.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    "banner" => "Automotive and Property News",
+    "banner" => "Automotive <br />and Property News",
     "banner_desc" => "Get the Latest Trends Here",
     "title" => "Your Source for Cutting-Edge News in Cars and Real Estate - Mediaoto",
     "description" => "Discover the pulse of automotive and property realms on Mediaoto.id. Uncover listings, updates, and trends in automotive and property, always ahead in news and prices!",
