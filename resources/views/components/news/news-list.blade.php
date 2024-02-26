6dec12b2a5f591fc6f46388ad69cdac58e1f3839
bobih <bobby.khrisna@gmail.com>
2024-02-26 07:03:20 +0700

    update

--- resources/views/components/news/news-list.blade.php
+++ resources/views/components/news/news-list.blade.php
@@ -1,5 +1,5 @@
 @props(['posts','categories','agent'])
-<video class="w-full" loop>
+<video class="w-full" controls loop>
     <source src="/videos/news.webm" type="video/webm">
     Your browser does not support the video tag.
   </video>
