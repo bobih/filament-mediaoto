123cb5c5aafa85102ee0c0af27862f52222b5ab4
bobih <bobby.khrisna@gmail.com>
2024-02-26 08:56:56 +0700

    update

--- resources/views/components/news/news-list.blade.php
+++ resources/views/components/news/news-list.blade.php
@@ -1,4 +1,6 @@
 @props(['posts','categories','agent'])
+
+@if ($agent->isMobile() == false)
 <div class="fixed w-full bottom-8 ">
     <video class="w-full " autoplay loop muted plays-inline>
         <source src="/videos/news3.webm" type="video/webm">
@@ -13,6 +15,7 @@
         </h1>
     </div>
 </div>
+@endif
 <section class="relative pt-8  mx-auto md:px-5 flex flex-grow bg-gray-100 dark:bg-gray-900">
 
     <div class="w-full max-w-screen-xl grid grid-cols-4 gap-4">
