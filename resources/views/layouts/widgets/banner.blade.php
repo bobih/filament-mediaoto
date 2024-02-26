1dff205befa9aa5dbe6da05c4511caa233becad8
bobih <bobby.khrisna@gmail.com>
2024-02-27 06:52:37 +0700

    update

--- resources/views/layouts/widgets/banner.blade.php
+++ resources/views/layouts/widgets/banner.blade.php
@@ -92,7 +92,7 @@
             <img data-te-lazy-load-init
                 data-te-lazy-placeholder="{{env('IMAGE_URL','https://www.mediaoto.id')}}/images/blank.png"
                 data-te-lazy-src="{{env('IMAGE_URL','https://www.mediaoto.id')}}/images/phone-mockup-mobile.webp"
-                data-te-lazy-animation="[zoom-in_1s_ease-in-out]"
+                data-te-lazy-animation="[fade-in_1s_ease-in-out]"
                 class="opacity-0" width="380" height="284" alt="mediaoto-apps" title="mediaoto-apps">
         </div>
 
