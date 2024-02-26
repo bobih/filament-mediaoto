ff57a238e6b2111b7eff90b75e7db22e7a89fa86
bobih <bobby.khrisna@gmail.com>
2024-02-26 10:46:16 +0700

    update

--- resources/views/components/news/news-list.blade copy.php
+++ resources/views/components/news/news-list.blade copy.php
@@ -6,7 +6,7 @@
 <div class="w-full">
     <div class="pl-12 text-start ">
 
-        <h1 data-te-animation-init="" data-te-animation-start="onScroll" data-te-animation-on-scroll="repeat" data-te-animation-show-on-load="false" data-te-animation-reset="true" data-te-animation="[fade-in_1s_ease-in-out]" class="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-5xl dark:text-white" style="visibility: visible;">
+        <h1  class="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-5xl dark:text-white" style="visibility: visible;">
         Berita Otomotif <br>dan Properti
         </h1>
         <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-200">
