d56a81328238d672e6a33656b4f470131bef7e85
bobih <bobby.khrisna@gmail.com>
2024-02-26 08:01:56 +0700

    update

--- resources/views/components/news/news-list.blade.php
+++ resources/views/components/news/news-list.blade.php
@@ -14,7 +14,6 @@
 
 
 </div>
-</div>
 <section class="relative pt-20  mx-auto md:px-5 flex flex-grow bg-gray-100 dark:bg-gray-900">
 
     <div class="w-full max-w-screen-xl grid grid-cols-4 gap-4">
