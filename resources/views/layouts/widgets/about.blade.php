7e42b7c1c00092f412a9182cb48149f3e5eb357b
bobih <bobby.khrisna@gmail.com>
2024-02-28 09:14:11 +0700

    update

--- resources/views/layouts/widgets/about.blade.php
+++ resources/views/layouts/widgets/about.blade.php
@@ -76,9 +76,7 @@
                     autoplay="true">
                 </x-lottie>
             </div>
-            <h2 class="mb-4 text-3xl tracking-tight font-bold text-gray-200 dark:text-white">
-                {{__('home.aboutus.aboutus')}}
-            </h2>
+
             <div class="pt-16 flex items-center text-gray-500 ">
 
 
@@ -164,6 +162,9 @@
 
             </div>
         </div>
+        <h2 class="mb-4 text-3xl tracking-tight font-bold text-gray-200 dark:text-white">
+            {{__('home.aboutus.aboutus')}}
+        </h2>
         <div class="mt-10 font-light text-gray-200 sm:text-lg dark:text-gray-300 lg:col-span-2">
             <p class="mb-4 text-justify opacity-0 intersect:opacity-100 transition-opacity duration-1000 delay-300 intersect-once">{{__('home.aboutus.content1')}}</p>
             <p class="mb-4 text-justify opacity-0 intersect:opacity-100 transition-opacity duration-1000 delay-300 intersect-once">{{__('home.aboutus.content2')}}</p>
