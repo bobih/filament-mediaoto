21c915efc751fe37522766f65962fa2e4fe9c9b6
bobih <bobby.khrisna@gmail.com>
2024-02-29 08:56:08 +0700

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -266,9 +266,9 @@ document.addEventListener('livewire:navigated', () => {
         loop: false,
         autoplay: false,
         path: '/images/anime/mediaoto_mobile.json',
-        onComplete: completedAnim
       });
       animation.play();
+      animation.addEventListener('complete', completedAnim);
 
       animation.addEventListener('onComplete',function(){
         console.log('heloooo');
