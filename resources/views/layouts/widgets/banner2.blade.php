cd8bf4723e89225fac1490b9a3bae5e034f54a75
bobih <bobby.khrisna@gmail.com>
2024-02-29 19:35:57 +0700

    update

--- resources/views/layouts/widgets/banner2.blade.php
+++ resources/views/layouts/widgets/banner2.blade.php
@@ -1,8 +1,8 @@
 <div id="banner" class="-mt-20 mb-20 md:mb-20"></div>
-<section class="pt-20 pb-10 bg-gray-100 dark:bg-gray-900 md:pb-12 z-10">
+<section class="h-[626px] pt-20 pb-10 bg-gray-100 dark:bg-gray-900 md:pb-12 z-10">
     @if ($agent->isMobile())
     <div class="flex w-full justify-between ">
-        <div id="bm" class="w-full h-[626px] justify-between fill-black bg:fill-white "
+        <div id="bm" class="-mt-20 w-full h-[626px] justify-between fill-black bg:fill-white "
             datasrc = "{{env('IMAGE_URL','https://www.mediaoto.id')}}/images/anime/mobile-6.json"
             title1='"Close the Deal"'
             title2="gak pake ribet!"
