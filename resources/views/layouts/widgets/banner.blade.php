2f4b6e514d9858d4ca3f5d310ed17408835dfa35
bobih <bobby.khrisna@gmail.com>
2024-02-29 17:06:52 +0700

    update

--- resources/views/layouts/widgets/banner.blade.php
+++ resources/views/layouts/widgets/banner.blade.php
@@ -3,7 +3,7 @@
     @if ($agent->isMobile())
     <div class="flex w-full justify-between ">
         <div id="bm" class="w-full h-[626px] justify-between fill-black bg:fill-white "
-            datasrc = "{{env('IMAGE_URL','https://www.mediaoto.id')}}/images/anime/mobile-5.json"
+            datasrc = "{{env('IMAGE_URL','https://www.mediaoto.id')}}/images/anime/mobile-4.json"
             title1='"Close the Deal"'
             title2="gak pake ribet"
             title3="Lacak tahapan dan pencapaian \r transaksi Anda. Nikmati penjadwalan yang efisien, dan follow-up prospek yang lebih lancar dengan Apps kami."
