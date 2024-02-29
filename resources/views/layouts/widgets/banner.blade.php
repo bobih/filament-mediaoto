eb055d94eaef44f6fc2ea626696a95a6c74926a8
bobih <bobby.khrisna@gmail.com>
2024-03-01 05:43:18 +0700

    update

--- resources/views/layouts/widgets/banner.blade.php
+++ resources/views/layouts/widgets/banner.blade.php
@@ -66,7 +66,7 @@
                 data-te-animation-reset="true"
                 data-te-animation="[fade-in_1s_ease-in-out]"
                 class=" max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-5xl dark:text-white">
-                    {!! str_replace(' ',trans('home.banner.infotag')) !!}
+                    {!! trans('home.banner.infotag') !!}
                 </h1>
                 <p  class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-200">{!! trans('home.banner.content1') !!}</p>
             <div class="hidden md:flex justify-between items-center">
