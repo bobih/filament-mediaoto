6f3692e3dfddb59678fdfae005b41187c1fa8cef
bobih <bobby.khrisna@gmail.com>
2024-03-01 12:25:51 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,7 +34,7 @@ export default {
                 ],
         extend: {
             dropShadow: {
-                '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+                'md': ['0 4px 10px -1px #00000052','0 2px 10px -2px #00000052'],
                 '4xl': [
                     '0 35px 35px rgba(0, 0, 0, 0.25)',
                     '0 45px 65px rgba(0, 0, 0, 0.15)'
