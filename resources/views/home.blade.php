5c315eeae1fdf85b93c76c360a9361ddbfdc26bd
bobih <bobby.khrisna@gmail.com>
2024-03-04 07:18:12 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
         @else
         @include('layouts.widgets.banner')
         @endif
-        <?php /*@include('layouts.widgets.about2') */ ?>
+        @include('layouts.widgets.about2')
         @include('layouts.widgets.about')
         @include('layouts.widgets.services')
         @include('layouts.widgets.products')
